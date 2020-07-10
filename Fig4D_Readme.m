2f1999a9a706159f781a262b53782eae56aca647
TomokoYamagata <51443393+TomokoYamagata@users.noreply.github.com>
2020-07-11 00:12:52 +0100

    Add files via upload

---
+++ Fig4D_Readme.m
@@ -0,0 +1,31 @@
+
+WakeEnhancement
+% 
+
+1.
+PlotHypnogram.m
+
+    Plot Hypnogram
+    Find latency to wake after 2 hour SD
+
+2.
+PlotHypnogramSleepOnset.m
+
+    Confirm latency to wake after 2 hour SD
+
+3.  
+SWAeffectSD.m     
+
+4.
+MeanSpectraEffectSD.m    
+    sub
+    MeanSpectraEffectSD_vsBaseline.m    
+
+5.
+Statistics
+    SleepAttemptsLatencySD.m     
+
+    
+
+    
+    
